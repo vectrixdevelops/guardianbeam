6cba6279a5e48ac7455da08d007e6be6920d13d5
Connor Hartley <vectrixu+gh@gmail.com>
2018-05-05 10:33:43 +1200

    Start report layout

--- src/views/Report.js
+++ src/views/Report.js
@@ -3,8 +3,12 @@ import { h } from 'hyperapp'
 
 const Report = (state, actions) =>
   h('div', { class: 'report-container' }, [
-    h('h1', {}, 'Report'),
-    h('h2', {}, 'Route: ' + state.Router.path)
+    h('div', { class: 'report-user' }, [
+
+    ]),
+    h('div', { class: 'report-details' }, [
+      h('div', { class: '' })
+    ])
   ])
 
 export default Report
